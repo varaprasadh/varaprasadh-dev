cb600baa6548d18c1b3e2241d57d1972567f88e9
varapradadh2 <varaprasadh.a@imgannovate.com>
2023-09-09 02:47:50 +0530

    update all entries

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'varaprasadh.dev',
   integrations: [mdx(), sitemap(), tailwind(), image(
     {
       serviceEntryPoint: '@astrojs/image/sharp',
