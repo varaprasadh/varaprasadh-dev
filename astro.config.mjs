587fdedb5aaa5159c0ffeb1736d2a8e55148b870
varapradadh2 <varaprasadh.a@imgannovate.com>
2023-09-09 23:35:03 +0530

    add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,17 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 import image from "@astrojs/image";
+
+// https://astro.build/config
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://varaprasadh-dev-varaprasadh.vercel.app/',
-  integrations: [mdx(), sitemap(), tailwind(), image(
-    {
-      serviceEntryPoint: '@astrojs/image/sharp',
-      cacheDir: "./.cache/image",
-      logLevel: 'debug',
-    }
-  )]
+  integrations: [mdx(), sitemap(), tailwind(), image({
+    serviceEntryPoint: '@astrojs/image/sharp',
+    cacheDir: "./.cache/image",
+    logLevel: 'debug'
+  }), react()]
 });
